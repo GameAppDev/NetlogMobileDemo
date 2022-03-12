4ec8460a63009a18865ad7fe78a98e6d7e1f50da
Oguzhan <oguzz.ylcn@gmail.com>
2022-03-12 12:54:21 +0300

    First commit

---
+++ NetlogMobileDemo/NetlogMobileDemo/ViewController.swift
@@ -0,0 +1,23 @@
+//
+// ViewController.swift
+// NetlogMobileDemo
+//
+// Created on 12.03.2022.
+// Oguzhan Yalcin
+//
+//
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
